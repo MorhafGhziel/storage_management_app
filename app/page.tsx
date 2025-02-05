0dca167cefea4d9fdfd31f16954cdc351fe9a010
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-02-06 01:54:39 +0300

    added basic link for sign in and sign up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex-center h-screen">
+    <div className="flex-center h-screen flex-col space-y-5">
       <h1 className="h1">StoreIt - The only storage solution you will need.</h1>
+      <p className="flex gap-5">
+        <a href="sign-in">Sign In</a>
+        <a href="sign-up">Sign Up</a>
+      </p>
     </div>
   );
 }
