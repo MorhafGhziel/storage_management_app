1b02b10657cdef7744aaaf009f04bf29d0dd4661
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-02-06 01:31:32 +0300

    added our shadcn form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,7 +1,69 @@
-import React from "react";
+"use client";
 
-const AuthForm = () => {
-  return <div>AuthForm</div>;
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+type FormType = "sign-in" | "sign-up";
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  // 1. Define your form inside AuthForm.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1>{type === "sign-in" ? "Sign In" : "Sign Up"}</h1>
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </>
+  );
 };
 
 export default AuthForm;
