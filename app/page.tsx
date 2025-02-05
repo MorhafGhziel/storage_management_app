972634c8e77bc006d9622e8ffb50ef95fcc1c607
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-02-05 21:18:24 +0300

    used our custom heading on the title

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div className="center">
-      <h1 className="text-3xl font-bold text-brand">
-        StoreIt - The only storage solution you will need.
-      </h1>
+      <h1 className="h1">StoreIt - The only storage solution you will need.</h1>
     </div>
   );
 }
